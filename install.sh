2d89da18a177305508b102fadfb88c3b007cd199
gexan <43960493+gexanlottery@users.noreply.github.com>
2019-04-29 10:59:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,9 +14,15 @@ GEXVERSION="2.0.0.0"
 EXTERNALIP=`dig +short myip.opendns.com @resolver1.opendns.com`
 clear
 
+TARBALLNAME="ubuntu16-daemon.tar.gz"
+GEXVERSION="2.0.0.0"
+# Get our current IP
+EXTERNALIP=`dig +short myip.opendns.com @resolver1.opendns.com`
+clear
+
 STRING1="Make sure you double check before hitting enter! Only one shot at these!"
 STRING2="If you found this helpful, please donate: "
-STRING3="XHEUZuevQUEuoPoiJSfMsNtV9DCkqZJhGP"
+STRING3="95r7Y1od6f4g2aZgNZrZR6eBTonsZat8Ay"
 STRING4="Updating system and installing required packages."
 STRING5="Switching to Aptitude"
 STRING6="Some optional installs"
@@ -44,14 +50,14 @@ echo $STRING4
 sleep 10
 
 # update package and upgrade Ubuntu
+sudo apt-get -y install software-properties-common
 sudo add-apt-repository -y ppa:bitcoin/bitcoin
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y purge qt4-qmake libqt4-dev
-sudo apt-get -y purge libdb++-dev libdb-dev
 sudo apt-get -y autoremove
 sudo apt-get -y install wget nano htop
-sudo apt-get -y install libdb4.8-dev libdb4.8++-dev libzmq3-dev libminiupnpc-dev libcrypto++-dev libboost-all-dev build-essential libboost-system-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libboost-filesystem-dev libboost-program-options-dev libboost-thread-dev libssl-dev ufw git software-properties-common libtool autotools-dev autoconf pkg-config libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev automake g++-mingw-w64-x86-64 libevent-dev bsdmainutils curl git unzip
+sudo apt-get -y install build-essential libtool bsdmainutils autotools-dev autoconf pkg-config automake python3 libssl-dev libgmp-dev libevent-dev libboost-all-dev libminiupnpc-dev libzmq3-dev 
+sudo apt-get -y install libdb4.8-dev libdb4.8++-dev unzip
 clear
 echo $STRING5
 sudo apt-get -y install aptitude
@@ -75,19 +81,20 @@ if [[ ("$UFW" == "y" || "$UFW" == "Y" || "$UFW" == "") ]]; then
   sudo ufw enable -y
 fi
 
-#Install GEXAN Daemon
+#Install GEX Daemon
 wget $TARBALLURL
-sudo unzip $TARBALLNAME
+sudo tar -xzvf $TARBALLNAME
 sudo rm $TARBALLNAME
 sudo cp gexd /usr/local/bin
 sudo cp gex-cli /usr/local/bin
-sudo cp gex-tx /usr/local/bin
 cd /usr/local/bin
 sudo chmod +x /usr/local/bin/gexd
 sudo chmod +x /usr/local/bin/gex-cli
-sudo chmod +x /usr/local/bin/gex-tx
 gexd -daemon
+sleep 60
+gex-cli stop
 clear
+cd ~/.Gexan && wget https://github.com/gexanlottery/gexan/releases/download/v2.0.0.0/bootstrap.zip && unzip bootstrap.zip
 
 #Setting up coin
 clear
@@ -98,21 +105,12 @@ echo $STRING13
 echo $STRING4
 sleep 10
 
-#Create Gexan.conf
+#Create gex.conf
 echo '
-rpcuser='$password'
-rpcpassword='$password2'
-rpcallowip=127.0.0.1
 listen=1
 server=1
 daemon=1
 logtimestamps=1
-maxconnections=256
-staking=0
-port=19666
-externalip='$EXTERNALIP'
-bind='$EXTERNALIP'
-masternodeaddr='$EXTERNALIP':19666
 masternodeprivkey='$key'
 masternode=1
 ' | sudo -E tee ~/.Gexan/Gexan.conf >/dev/null 2>&1
