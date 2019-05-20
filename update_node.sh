f48463ae3fd87eeb47366e250bedbb9944267d7f
gexan <43960493+gexanlottery@users.noreply.github.com>
2019-05-20 20:50:40 +0300

    Update update_node.sh

--- update_node.sh
+++ update_node.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-TARBALLURL="https://github.com/gexanlottery/gexan/releases/download/untagged-7d961a8735ca76df2c42/ubuntu16.04-daemon.zip"
+TARBALLURL="https://github.com/gexanlottery/gexan/releases/download/v2.0.3.1/ubuntu16.04-daemon.zip"
 TARBALLNAME="ubuntu16.04-daemon.zip"
-GEXVERSION="2.0.0.0"
+GEXVERSION="2.0.3.1"
 
 clear
-echo "This script will update your masternode to version 2.0.0.0"
+echo "This script will update your masternode to version 2.0.3.1"
 read -p "Press Ctrl-C to abort or any other key to continue. " -n1 -s
 clear
 echo "Please enter your password to enter administrator mode:"
 sudo true
 echo "Shutting down masternode..."
 gex-cli stop
-echo "Installing Gexan 2.0.0.0"
+echo "Installing Gexan 2.0.3.1"
 mkdir ./gex-temp && cd ./gex-temp
 wget $TARBALLURL
 unzip $TARBALLNAME
 sudo rm $TARBALLNAME
 yes | sudo cp -rf gexd /usr/local/bin
 yes | sudo cp -rf gex-cli /usr/local/bin
-yes | sudo cp -rf gex-tx /usr/local/bin
 sudo chmod +x /usr/local/bin/gexd
 sudo chmod +x /usr/local/bin/gex-cli
-sudo chmod +x /usr/local/bin/gex-tx
 cd ..
 sed -i '/^addnode/d' ~/.gex/gex.conf
 cat <<EOL >>  ~/.Gexan/Gexan.conf
